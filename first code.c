65e1e0da61dd50979df63e09b023344375539dfe
shahsneh859 <123948975+shahsneh859@users.noreply.github.com>
2023-01-30 17:14:39 +0530

    Update first code.c

--- first code.c
+++ first code.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <conio.h>
+//Creating main class
 int getValue(const char * s){
     switch(*s) {
-        case 'I': return (s[1] == 'V' || s[1] == 'X') ? -1 : 1;
-        case 'X': return (s[1] == 'L' || s[1] == 'C') ? -10 : 10;
-        case 'C': return (s[1] == 'D' || s[1] == 'M') ? -100 : 100;
+        case 'I': return 1;
         case 'V': return 5;
+        case 'X': return 10;   
         case 'L': return 50;
+        case 'c': return 100;
         case 'D': return 500;
         case 'M': return 1000;
     }
     return 0;
 }
-
+//Output
 int romanToInt(char * s){
     int result = 0; 
     
