9a27dd6aec84bb1c61779ec618e1133071b102cd
shahsneh859 <123948975+shahsneh859@users.noreply.github.com>
2023-01-30 17:18:03 +0530

    Create firstcode.py

---
+++ firstcode.py
@@ -0,0 +1,14 @@
+def romanToInt(s: str) -> int:
+    # Dictionary of roman numerals
+    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    # Length of the given string
+    n = len(s)
+    num = roman_map[s[n - 1]]
+    # Loop for each character from right to left
+    for i in range(n - 2, -1, -1):
+       
+        if roman_map[s[i]] >= roman_map[s[i + 1]]:
+            num += roman_map[s[i]]
+        else:
+            num -= roman_map[s[i]]
+    return num
